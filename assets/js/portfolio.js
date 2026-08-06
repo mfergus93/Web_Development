e98a8b2c10e8f89a13a88f28525b308d5baf834d
mfergus93 <mcferguson9@gmail.com>
2026-08-05 19:24:46 -0500

    Move photography to dedicated tab page

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -12,27 +12,8 @@ function openLinkedSection(hash) {
 	if (target instanceof HTMLDetailsElement) target.open = true;
 }
 
-function updatePageTabs(hash) {
-	const selectedTab = hash === "#photography" ? "photography" : "professional";
-	document.querySelectorAll(".page-tabs [data-tab]").forEach((tab) => {
-		if (tab.dataset.tab === selectedTab) {
-			tab.setAttribute("aria-current", "page");
-		} else {
-			tab.removeAttribute("aria-current");
-		}
-	});
-}
-
 document.querySelectorAll('a[href^="#"]').forEach((link) => {
-	link.addEventListener("click", () => {
-		openLinkedSection(link.hash);
-		updatePageTabs(link.hash);
-	});
+	link.addEventListener("click", () => openLinkedSection(link.hash));
 });
 
 openLinkedSection(window.location.hash);
-updatePageTabs(window.location.hash);
-window.addEventListener("hashchange", () => {
-	openLinkedSection(window.location.hash);
-	updatePageTabs(window.location.hash);
-});
